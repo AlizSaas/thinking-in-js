34fa6c8bbaee5266ce52cb700e7fdaa54e210864
AlizSaas <blackbearddevss@gmail.com>
2026-07-04 15:19:03 -0400

    another day

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-function bankAccount(initalBalance) {
-    var balance = initalBalance;
-
-    return  function () {
-        return balance;
-    }
-
-
+function add(...x) {
+   return x.reduce((acc, curr) => acc + curr, 0);
 }
 
-var account = bankAccount(100);
-console.log(account()); // 100
+
+
+function memo(func) {
+    const cached  = {}
+    console.log('cached', cached);
+    return function (...x) {
+      const key = JSON.stringify(x);
+        if(cached[key]) {
+            console.log('result from cache');
+            return cached[key];
+        } else {
+            console.log('Calculating result');
+            const result = func(...x);
+            cached[key ] = result;
+            return result;
+        }
+
+        
+
+    }
+}
+
+const calculate = memo(add);
+
+console.log(calculate(5,10,15,20)); 
+console.log(calculate(5));
+ 
+
+
+
